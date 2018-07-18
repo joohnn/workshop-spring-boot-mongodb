ca5b6f7b39e559199315733e82448b0175c3e595
João Simão <joao.simao@fatec.sp.gov.br>
2018-07-17 23:48:19 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/jonsimao/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.jonsimao.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.jonsimao.workshopmongo.domain.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String>{
+
+	
+}
--- src/main/java/com/jonsimao/workshopmongo/resources/UserResource.java
+++ src/main/java/com/jonsimao/workshopmongo/resources/UserResource.java
@@ -4,25 +4,26 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.jonsimao.workshopmongo.domain.User;
+import com.jonsimao.workshopmongo.services.UserService;
 
 @RestController
 @RequestMapping(value="/users")
 public class UserResource {
 	
+	@Autowired
+	private UserService service;
+	
 	@RequestMapping(method=RequestMethod.GET)
 	/*@GetMapping -- É possivel usar isto no lugar do de cima*/
 	public ResponseEntity<List<User>> findAll(){
-		User maria = new User("1", "Maria Brown", "maria@gmail.com");
-		User alex = new User("2", "Alex Green", "alex@gmail.com");
-		User jon = new User("3", "Jon Simão", "jon@jon.net");
-		List<User> list = new ArrayList<>();
-		list.addAll(Arrays.asList(maria, alex, jon));		
+		List<User> list = service.findAll();		
 		return ResponseEntity.ok().body(list);		
 	}
 
